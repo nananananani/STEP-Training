1a26fd13b5ae1e603127822ba0b8c091872b9fdd
nananananani <ayuuki478@gmail.com>
2025-08-20 09:01:56 +0530

    Week1 Length of String Final

--- Week1Lab/LengthofString.java
+++ Week1Lab/LengthofString.java
@@ -13,7 +13,8 @@ public class LengthofString {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = input.next();
-        System.out.println("Length of the string: " + findlength(str));
+        System.out.println("Length of the string using user defined function: " + findlength(str));
+        System.out.println("Length of the string using inbuilt function: " + str.length());
 
     }
 
