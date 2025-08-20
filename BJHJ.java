db60a4c18f273743070b237abc728ff735d0c6df
nananananani <ayuuki478@gmail.com>
2025-08-20 08:29:47 +0530

    Initial commit

---
+++ BJHJ.java
@@ -0,0 +1,7 @@
+public class BJHJ {
+    public static void main(String[] args) {
+        // This is a placeholder for the main method.
+        // You can add your code logic here.
+        System.out.println("Hello, BJHJ!");
+    }
+}
