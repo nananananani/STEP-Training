f62f5e525ca322cf85e8c67c8845d5c0e269539d
nananananani <ayuuki478@gmail.com>
2025-08-20 08:57:16 +0530

    Week1 Lab: Length of String

---
+++ Week1Lab/LengthofString.java
@@ -0,0 +1,21 @@
+package Week1Lab;
+import java.util.Scanner;
+
+public class LengthofString {
+    static int findlength(String str) {
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+        return count;
+    }
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = input.next();
+        System.out.println("Length of the string: " + findlength(str));
+
+    }
+
+    
+}
