bc327c1051042453d45fa34142fed5875f3b33c2
nananananani <ayuuki478@gmail.com>
2025-08-27 13:55:27 +0530

    updation

--- BJHJ.java
+++
@@ -1,7 +0,0 @@
-public class BJHJ {
-    public static void main(String[] args) {
-        // This is a placeholder for the main method.
-        // You can add your code logic here.
-        System.out.println("Hello, BJHJ!");
-    }
-}
---
+++ Week1Lab/Splitwords.java
@@ -0,0 +1,14 @@
+package Week1Lab;
+import java.util.Scanner;
+
+public class Splitwords {
+    static int findlength(String str){
+        int count = 0;
+         for (char c : str.toCharArray()){
+            count++;
+         }
+         return count;
+    }
+    static void splitwords
+    
+}
